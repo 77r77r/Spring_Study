4dfbb5278db9a519999e9a32962c69175e826e54
77r77r <32328758+77r77r@users.noreply.github.com>
2025-05-24 23:30:12 +0900

    2025.05.24_Welcome Page 만들기

--- src/main/java/com/iwon/hello_spring/controller/HelloController.java
+++ src/main/java/com/iwon/hello_spring/controller/HelloController.java
@@ -10,7 +10,7 @@ public class HelloController {
     @GetMapping("hello")
     public String hello(Model model) {
         // 모델 객체에 data:hello!! 추가하기
-        model.addAttribute("data", "hello!!");
+        model.addAttribute("data", "Controller의 model 값");
         return "hello"; // src/main/resources/templates/hello.html
         // return "ViewName"; -> resources:templates/ +{ViewName}+ .html
     }
